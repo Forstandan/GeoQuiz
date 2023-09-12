82320ce8a916ab5e0c3e3f9dbf55fad4429c5ede
Forstandan <114364542+Forstandan@users.noreply.github.com>
2023-09-11 17:07:44 -0700

    Update

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizVewModel.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizVewModel.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import kotlin.math.abs
 
 private const val TAG = "QuizViewModel"
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
@@ -10,12 +11,12 @@ const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
 class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
+        Question(R.string.question_sanskrit, true),
+        Question(R.string.question_german, false),
+        Question(R.string.question_arabic, false),
+        Question(R.string.question_magyar, false),
+        Question(R.string.question_thirtyYearsWar, true),
+        Question(R.string.question_sweden, true)
     )
 
     var isCheater: Boolean
@@ -36,6 +37,9 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
     }
 
     fun moveToPrev() {
-        currentIndex = kotlin.math.abs(currentIndex - 1) % questionBank.size
+        currentIndex = if (currentIndex > 0)
+            (currentIndex - 1) % questionBank.size
+        else
+            questionBank.size-1;
     }
 }
